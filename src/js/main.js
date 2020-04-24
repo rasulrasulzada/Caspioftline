6eb0daf50ac3046c3176b1cbe4e9ebb348e4cf1e
rasulrasulzada <rasul.rasulzada1@gmail.com>
2020-04-24 21:22:15 +0400

    added sticky navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,45 @@
-import "../styles/main.sass"
+import "../styles/main.sass";
 
-'use strict'
-import {ableSearch, disableSearch,toggleNav,toDown} from './homepage'
-import lazyload from "./lazyload"
-import {sendIdea,ideaForm} from "./sendIdea"
-import slickSlider from "./slickSlider"
-import {careersAccordion,fileInput, handleCareersForm} from "./careers"
-import {inputContactFocus,handleMessageForm} from "./contact"
-import {inputFaqFocus,faqAcc,handleQuestionForm} from "./faq"
+("use strict");
+import { ableSearch, disableSearch, toggleNav, toDown } from "./homepage";
+import lazyload from "./lazyload";
+import { sendIdea, ideaForm } from "./sendIdea";
+import slickSlider from "./slickSlider";
+import { careersAccordion, fileInput, handleCareersForm } from "./careers";
+import { inputContactFocus, handleMessageForm } from "./contact";
+import { inputFaqFocus, faqAcc, handleQuestionForm } from "./faq";
 
-$(document).ready(function(){
-lazyload()
+$(document).ready(function () {
+  lazyload();
 
- ableSearch();
- disableSearch()
- toggleNav();
- toDown();
- sendIdea();
- ideaForm();
- slickSlider();
+  ableSearch();
+  disableSearch();
+  toggleNav();
+  toDown();
+  sendIdea();
+  ideaForm();
+  slickSlider();
 
+  careersAccordion();
+  fileInput();
+  handleCareersForm();
 
- careersAccordion();
- fileInput();
- handleCareersForm();
+  inputFaqFocus();
+  faqAcc();
+  handleQuestionForm();
 
- 
+  inputContactFocus();
+  handleMessageForm();
 
- inputFaqFocus();
- faqAcc();
- handleQuestionForm();
-
- inputContactFocus();
- handleMessageForm();
-
- 
+  const stickyNav = () => {
+    window.addEventListener("scroll", function () {
+      const nav = document.querySelector("nav");
+      if (window.scrollY > nav.offsetTop) {
+        nav.classList.add("sticky-navbar");
+      } else {
+        nav.classList.remove("sticky-navbar");
+      }
+    });
+  };
+  stickyNav();
 });
